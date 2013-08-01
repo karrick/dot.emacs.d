9c370cfdd09ab011401767a5c766b445d49d0900
Karrick McDermott <karrick@karrick.net>
2013-07-31 21:54:31 -0400

    added ispell

--- init.el
+++ init.el
@@ -258,3 +258,16 @@
 
 ;; Zenburn
 (load-theme 'zenburn t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ispell-program-name "ispell"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

0ff2d61cad477da80f224ebd1eb33677ce03b08b
Karrick McDermott <karrick@gmail.com>
2014-01-10 13:44:01 -0500

    tested versions 22, 23, 24

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 ;;;; -*- mode: emacs-lisp -*-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; attempt to use package manager, but gracefully fail to an error
-;; message
+;;;; attempt to use package manager, but employ graceful degredation
+;;;; where elpa is unavailable
 
 (condition-case err
     (progn
@@ -32,22 +32,18 @@
           (message "skipping configure-package: %s" pkg))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; I don't have any package specific configuration for these packages
+;;;; I don't have any package specific configuration for these packages
 
 (configure-package '(
 		  bash-completion
-		  edit-server
-		  expand-region
 		  fic-mode
 		  find-file-in-repository
 		  go-autocomplete
 		  go-mode
-		  js2-mode
 		  json-mode
 		  magit
 		  markdown-mode
 		  maxframe
-		  multiple-cursors
 		  nxml-mode
 		  psgml
 		  puppet-mode
@@ -55,39 +51,7 @@
 		  smart-tab
 		  yaml-mode
 		  zenburn-theme
-		  flubber
 		  ))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(if (boundp 'user-emacs-directory)
-    (add-to-list 'load-path user-emacs-directory)
-  (add-to-list 'load-path (expand-file-name "~/.emacs.d")))
-
-(cd (expand-file-name "~"))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Language specific setup files
-;; (eval-after-load 'js2-mode '(require 'setup-js2-mode))
-(eval-after-load 'ruby-mode '(require 'setup-ruby-mode))
-;; (eval-after-load 'clojure-mode '(require 'setup-clojure-mode))
-;; (eval-after-load 'markdown-mode '(require 'setup-markdown-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; git configuration
-(setenv "GIT_PAGER" "")			; elide git paging capability
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ansi-color
-
-(require 'ansi-color)
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-(add-hook 'compilation-mode-hook 'ansi-color-for-comint-mode-on)
-(defun colorize-compilation-buffer ()
-  (toggle-read-only)
-  (ansi-color-apply-on-region compilation-filter-start (point-max))
-  (toggle-read-only))
-(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -102,20 +66,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setenv "GOPATH" (expand-file-name "~/go"))
-(prepend-path (expand-file-name "~/go/bin"))
-(prepend-path "/usr/local/go/bin")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(let ((directories (list
-                    "~/bin"
-                    "/usr/local/linkedin/bin"
-                    )))
-  (dolist (dir directories)
-    (prepend-path dir)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun find-first (predicate list)
   "Return result of first item in list which
 satisfies predicate.  Returns nil if predicate
@@ -126,19 +76,38 @@ is nil for all items in list."
 	(if result
 	    (throw 'found-it result))))))
 
-;;;;;;;;;;;;;;;;
-(let ((client (find-first #'(lambda (item)
-                              (executable-find item))
-                          '(
-                            "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient"
-                            "/usr/local/bin/emacsclient"
-                            "/usr/bin/emacsclient"))))
-  (setenv "EDITOR" client)
-  (setenv "VISUAL" client))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun other-window-backwards (&optional n)
+  "Select Nth previous window"
+  (interactive "p")
+  (other-window (- (prefix-numeric-value n))))
+
+(global-set-key "\C-x\C-n" 'other-window)
+(global-set-key "\C-x\C-p" 'other-window-backwards)
+(global-set-key "\C-xn" 'other-window)
+(global-set-key "\C-xp" 'other-window-backwards)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; process environment
+
+(let ((directories (list
+                    "~/bin"
+                    "/usr/local/linkedin/bin"
+                    )))
+  (dolist (dir directories)
+    (prepend-path dir)))
+
+(if (boundp 'user-emacs-directory)
+    (add-to-list 'load-path user-emacs-directory)
+  (add-to-list 'load-path (expand-file-name "~/.emacs.d")))
+
+(cd (expand-file-name "~"))
+(setenv "GIT_PAGER" "")			; elide git paging capability
 (setenv "PAGER" "cat")
 
-;; ________________________________________
-;; save new scripts as executable
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; save new scripts as executable
 
 (add-hook 'after-save-hook
           #'(lambda ()
@@ -157,7 +126,8 @@ is nil for all items in list."
                  "Wrote and made executable: %s" buffer-file-name))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; Advise the shell commands to name the buffer after the command itself
+;;;; advise the shell commands to name the buffer after the command
+;;;; itself
 
 (defadvice async-shell-command (before buffer-named-with-command
                                        (command &optional output-buffer error-buffer)
@@ -175,12 +145,14 @@ is nil for all items in list."
     (switch-to-buffer output-buffer)
     (setq default-directory dir)))
 
+(when (fboundp 'async-shell-command)
+  (global-set-key [(meta !)] 'async-shell-command))
+
 ;;;; auto-complete-mode
 (configure-package '(auto-complete)
 		   (require 'auto-complete-config)
 		   (add-to-list 'ac-dictionary-directories
 				(expand-file-name "~/.ac-dict"))
-
 		   ;; ac-common-setup is called by ac-config-default
 		   (defun ac-common-setup ()
 		     (add-to-list 'ac-sources 'ac-source-yasnippet))
@@ -192,78 +164,66 @@ is nil for all items in list."
 		   (defun disable-auto-complete-mode ()
 		     (auto-complete-mode 0)))
 
-;;;; edit-server for chromium browsers
-(when (and (fboundp 'daemonp) (daemonp) (locate-library "edit-server"))
-  (require 'edit-server)
-  (setq edit-server-new-frame nil)
-  (edit-server-start))
+;;;; edit-server for browsers
+;; install "It's All Text!" on Firefox, or "Edit with Emacs" for Chrome
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; tabs and indenting
+(configure-package '(edit-server)
+                   (when (and (fboundp 'daemonp) (daemonp) (locate-library "edit-server"))
+                     (require 'edit-server)
+                     (setq edit-server-new-frame nil)
+                     (edit-server-start)
+                     (let ((client (find-first #'(lambda (item)
+                                                   (executable-find item))
+                                               '(
+                                                 "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient"
+                                                 "/usr/local/bin/emacsclient"
+                                                 "/usr/bin/emacsclient"))))
+                       (setenv "EDITOR" client)
+                       (setenv "VISUAL" client))))
+
+;;;; tabs and indenting
+
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 8)
 
-;;;; js2 indent 2 spaces
-(set-default 'js2-basic-offset 2)
-(set-default 'js2-mirror-mode  nil)
-(set-default 'js2-mode-escape-quotes  nil)
+;;;; ido-mode
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; markdown mode
-
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ruby mode
-
-(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\Rakefile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\Gemfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; puppet mode
-
-(add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; xslt mode
-
-(add-to-list 'auto-mode-alist '("\\.xslt\\'" . nxml-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ido-mode
 (require 'ido)
 (ido-mode t)
 (setq ido-enable-flex-matching t)	; enable fuzzy matching
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; different way of uniquifying names
+;;;; different way of uniquifying names
+
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward)
 (setq uniquify-after-kill-buffer-p nil)
 (setq uniquify-ignore-buffers-re "^\\*")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; SVN mode
-(require 'psvn)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (prefer-coding-system 'utf-8)
-(setq diff-switches "-u")		; default to unified diffs
+(setq diff-switches "-u")
 (setq dired-listing-switches "-Bhl")
 (setq dired-show-ls-switches t)
-(setq ediff-diff-options "-w")
-(setq make-backup-files nil)		; stop making backup files
-(setq compilation-scroll-output 'first-error) ; compilation mode scrolls to first error
+(setq make-backup-files nil)
+(setq compilation-scroll-output 'first-error)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; look and feel
+;;;; svn mode
 
-;; Disable bars
+(require 'psvn)
+
+;;;; ansi-color
+(require 'ansi-color)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+(add-hook 'compilation-mode-hook 'ansi-color-for-comint-mode-on)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region compilation-filter-start (point-max))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+(ansi-color-for-comint-mode-on) ; allow terminal colorization
+
+;;;; disable bars
 (if (fboundp 'tool-bar-mode)
     (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode)
@@ -274,36 +234,34 @@ is nil for all items in list."
 (if (boundp 'desktop-save-mode)
     (desktop-save-mode 0))		; don't save desktop sessions
 
-;;;; Highlight entire expression within parens
+;;;; highlight entire expression within parens
 (show-paren-mode t)
 (setq show-paren-style 'expression)
 (set-face-background 'show-paren-match-face "#1f3f3f")
 
-;;;; Set to a usable font
+;;;; set to a usable font
 (add-to-list 'default-frame-alist '(font-backend . "xft"))
-;; (add-to-list 'default-frame-alist '(font . "Liberation Mono-12"))
 (setq font-use-system-font t)
+(condition-case err
+    (add-to-list 'default-frame-alist '(font . "Liberation Mono-12"))
+  (error nil))
 
-;;;; Don't spawn a new frame for the ediff commands, keep it all in one frame
+;;;; ediff
+
+(setq ediff-diff-options "-w")
+;;;; don't spawn a new frame for the ediff commands, keep it all in one frame
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
-;;;; Have ediff buffers show in a side-by-side view
+;;;; have ediff buffers show in a side-by-side view
 (setq ediff-split-window-function 'split-window-horizontally)
 
-;;;; Add line and column numbers to the modeline
+;;;; add line and column numbers to the modeline
 (line-number-mode 1)
 (column-number-mode 1)
 (setq scroll-conservatively 5)
 (setq scroll-step 1)
 (setq visible-bell 1)
 (setq inhibit-startup-message t)
-(ansi-color-for-comint-mode-on) ; Allow terminal colorization
 (put 'narrow-to-region 'disabled nil)
-
-;; other-window-backwards
-(defun other-window-backwards (&optional n)
-  "Select Nth previous window"
-  (interactive "p")
-  (other-window (- (prefix-numeric-value n))))
 
 ;;;; key bindings
 
@@ -311,56 +269,100 @@ is nil for all items in list."
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-x C-r") 'rgrep)
-(global-set-key [(meta !)] 'async-shell-command)
 (global-set-key [(meta g)] 'goto-line)
 (global-set-key [f5] '(lambda () (interactive) (revert-buffer nil t nil)))
-(global-set-key "\C-x\C-n" 'other-window)
-(global-set-key "\C-x\C-p" 'other-window-backwards)
-(global-set-key "\C-xn" 'other-window)
-(global-set-key "\C-xp" 'other-window-backwards)
-
-;;;; expand-region
-(global-set-key (kbd "H-=") 'er/expand-region)
-(global-set-key (kbd "H--") 'er/contract-region)
-
-;;;; multiple-cursor mode
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-c C->") 'mc/mark-more-like-this-extended)
-
-;;;; set f8 to be recompile, shift-f8 to compile
 (global-set-key [f8]   'recompile)
 (global-set-key [S-f8] 'compile)
 
+;;;; expand-region
+
+(configure-package '(expand-region)
+                   (global-set-key (kbd "H-=") 'er/expand-region)
+                   (global-set-key (kbd "H--") 'er/contract-region))
+
+;;;; multiple-cursor mode
+
+(configure-package '(multiple-cursors)
+                   (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+                   (global-set-key (kbd "C-c C-S-c") 'mc/edit-lines)
+                   (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+                   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+                   (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+                   (global-set-key (kbd "C-c C->") 'mc/mark-more-like-this-extended))
+
 ;;;; writable grep buffers via toggling off read-only (similar to
 ;;;; wdired mode for dired buffers)
+
 (configure-package '(wgrep wgrep-ack)
                    (require 'wgrep)
                    (define-key grep-mode-map (kbd "C-x C-q") 'wgrep-change-to-wgrep-mode)
                    (setq wgrep-auto-save-buffer t))
 
-;; don't let the cursor go into minibuffer prompt (thank's, xah!)
+;;;; don't let the cursor go into minibuffer prompt (thank's, xah!)
+
 (setq minibuffer-prompt-properties
       (quote (read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)))
 
-;; Zenburn
+;;;; zenburn
+
 (configure-package '(zenburn-theme)
                    (load-theme 'zenburn t))
 
 ;;;; Darwin fixes
-;; TODO: want to run some of these localized to where frame is created, and upon creation of new frame
+;;;; TODO: want to run some of these localized to where frame is created, and upon creation of new frame
 
 (when (eq system-type 'darwin)
-  (setq ns-function-modifier 'hyper)
   ;; (menu-bar-mode 1)
   ;; (setenv "LANG" "en_US.UTF-8")
-  (setq dired-use-ls-dired nil)
-  ;; (setenv "PATH" "/Users/mjcurry/bin:/opt/local/libexec/gnubin:/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/local/bin:/opt/local/libexec/gnubin:/usr/X11/bin")
-)
+  (setq ns-function-modifier 'hyper)
+  (setq dired-use-ls-dired nil))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Language specific setup files
+;;;; (eval-after-load 'clojure-mode '(require 'setup-clojure-mode))
+;;;; (eval-after-load 'markdown-mode '(require 'setup-markdown-mode))
+
+;;;; js2-mode offers nice javascript support
+(configure-package '(js2-mode)
+                   (eval-after-load 'js2-mode '(require 'setup-js2-mode))
+                   (set-default 'js2-basic-offset 2)
+                   (set-default 'js2-mirror-mode  nil)
+                   (set-default 'js2-mode-escape-quotes  nil))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; markdown mode
+
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; ruby mode
+
+(eval-after-load 'ruby-mode '(require 'setup-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Rakefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; puppet mode
+
+(add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; xslt mode
+
+(add-to-list 'auto-mode-alist '("\\.xslt\\'" . nxml-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; golang
+
+(setenv "GOPATH" (expand-file-name "~/go"))
+(prepend-path (expand-file-name "~/go/bin"))
+(prepend-path "/usr/local/go/bin")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

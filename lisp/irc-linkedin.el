5a35ec8554aaece850b88bc6c2ca0fb0a292140e
Karrick McDermott <kmcdermott@linkedin.com>
2015-01-30 08:39:41 -0500

    prune the channels to automatically join

--- lisp/irc-linkedin.el
+++ lisp/irc-linkedin.el
@@ -4,34 +4,20 @@
 (add-to-list 'erc-keywords "\\bkmcdermo\\b")
 (add-to-list 'erc-autojoin-channels-alist
              '(".*\.corp\.linkedin\.com" . (
-                                            "#metrics"
-                                            ;; "#alert-onboarding"
                                             "#ansible"
-                                            ;; "#central"
                                             "#coresre"
-                                            ;; "#couchbase"
-                                            ;; "#databus"
-                                            ;; "#dba"
                                             "#go"
                                             "#infra"
                                             "#metrics"
-                                            ;; "#money"
-                                            ;; "#netops"
                                             "#noc"
                                             "#nyc"
                                             "#nyc-sre"
                                             "#python"
-                                            ;; "#release"
-                                            "#salt"
                                             "#sand"
                                             "#secsre"
                                             "#security"
-                                            ;; "#siteops"
                                             "#sre"
                                             "#sre-infra"
-                                            ;; "#sysops"
-                                            ;; "#tools-sre"
-                                            ;; "#trafficshift"
                                             )))
 
 (provide 'irc-linkedin)

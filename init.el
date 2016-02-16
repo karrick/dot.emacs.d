da0ce6a13a265c7544cfd846b0eae68f608bf68b
Karrick S. McDermott <karrick@karrick.net>
2016-02-16 17:43:47 -0500

    uses emacs' kbd macro to define windmove keys

--- init.el
+++ init.el
@@ -325,10 +325,10 @@ is nil for all items in list."
 
 ;;; move to different window
 
-(global-set-key [(meta up)] 'windmove-up)
-(global-set-key [(meta down)] 'windmove-down)
-(global-set-key [(meta left)] 'windmove-left)
-(global-set-key [(meta right)] 'windmove-right)
+(global-set-key (kbd "M-<down>") 'windmove-down)
+(global-set-key (kbd "M-<left>") 'windmove-left)
+(global-set-key (kbd "M-<right>") 'windmove-right)
+(global-set-key (kbd "M-<up>") 'windmove-up)
 
 ;;;; expand-region
 

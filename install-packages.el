416579a37ab02f2949185f2fc6fbbd085af0fd10
Karrick S. McDermott <kmcdermott@linkedin.com>
2016-07-13 12:12:19 -0600

    uses go-guru rather than go-oracle

--- install-packages.el
+++ install-packages.el
@@ -1,51 +1,50 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("gnu" . "https://elpa.gnu.org/packages/"))
+	     '("gnu" . "https://elpa.gnu.org/packages/"))
 ;; (add-to-list 'package-archives
 ;;              '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa" . "https://melpa.org/packages/"))
 (setq package-enable-at-startup nil)
 (when (not package-archive-contents)
   (package-refresh-contents))
 (package-initialize)
 
 (dolist (pkg '(
-               auto-complete
-               auto-complete-config
-               bash-completion
-               edit-server
+	       auto-complete
+	       auto-complete-config
+	       bash-completion
+	       edit-server
                erc
-               expand-region
-               fic-mode
-               find-file-in-repository
-               flycheck
-               go-autocomplete
-               go-mode
+	       expand-region
+	       fic-mode
+	       find-file-in-repository
+	       flycheck
+	       go-autocomplete
+	       go-mode
                ;; go get -u github.com/google/codesearch/cmd/...
                ;; go get -u github.com/nsf/gocode
                ;; go get -u github.com/rogpeppe/godef
                ;; go get -u golang.org/x/tools/cmd/goimports
-               ;; go get -u golang.org/x/tools/cmd/oracle
-               go-rename ;; go get -u golang.org/x/tools/cmd/gorename
-               golint ;; go get -u github.com/golang/lint/golint
-               js2-mode
-               json-mode
-               markdown-mode
-               maxframe
-               multiple-cursors
-               nxml-mode
-               ;; psgml
-               puppet-mode
-               rust-mode
-               shell-command
-               smart-tab
-               wgrep
-               wgrep-ack
-               yaml-mode
-               zenburn-theme
-               ))
+               ;; go get -u golang.org/x/tools/cmd/guru
+	       go-rename ;; go get -u golang.org/x/tools/cmd/gorename
+	       golint ;; go get -u github.com/golang/lint/golint
+	       js2-mode
+	       json-mode
+	       markdown-mode
+	       maxframe
+	       multiple-cursors
+	       nxml-mode
+	       ;; psgml
+	       puppet-mode
+	       shell-command
+	       smart-tab
+	       wgrep
+	       wgrep-ack
+	       yaml-mode
+	       zenburn-theme
+		))
   (if (not (package-installed-p pkg))
       (condition-case err
-          (package-install pkg)
-        (error (message "Cannot install package: %s" err)))))
+	  (package-install pkg)
+	(error (message "Cannot install package: %s" err)))))
--- lisp/setup-go-mode.el
+++ lisp/setup-go-mode.el
@@ -5,31 +5,39 @@
 (require 'go-autocomplete)
 
 (let ((gopath (expand-file-name "~/go")))
-  (when (file-exists-p gopath)
-    (prepend-path (concat gopath "/bin"))
-    (setenv "GOPATH" gopath)
-    (setenv "GO15VENDOREXPERIMENT" "1")
-    (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
-    (load (concat gopath "/src/golang.org/x/tools/cmd/oracle/oracle"))
-    (add-hook 'go-mode-hook
-              #'(lambda ()
-                  (add-hook 'before-save-hook #'gofmt-before-save nil t)
-                  (flyspell-prog-mode)
-                  (setq fill-column 100)
-                  (local-set-key (kbd "C-c C-j") 'godef-jump-other-window)
-                  (if (not (string-match "^go" compile-command))
-                      (set (make-local-variable 'compile-command)
-                           "go test && go build"))))
-    (eval-after-load "go-mode"
-      #'(lambda ()
-          (let ((gofmter (find-first #'(lambda (item)
-                                         (executable-find item))
-                                     '("goimports"
-                                       "gofmt"))))
-            (setq gofmt-command gofmter)))))
+  (prepend-path (concat gopath "/bin"))
+  (setenv "GOPATH" gopath)
+  (setenv "GO15VENDOREXPERIMENT" "1")   ; won't need this much longer
+  (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+  (eval-after-load "go-mode"
+    #'(lambda ()
+        (let ((gofmter (find-first #'(lambda (item)
+                                       (executable-find item))
+                                   '("goimports"
+                                     "gofmt"))))
+          (setq gofmt-command gofmter))
+        (add-hook 'go-mode-hook
+                  #'(lambda ()
+                      (add-hook 'before-save-hook #'gofmt-before-save nil t)
+                      (flyspell-prog-mode)
+                      (setq fill-column 100)
+                      ;; (local-set-key (kbd "C-c C-j") 'godef-jump-other-window)
+                      (if (not (string-match "^go" compile-command))
+                          (set (make-local-variable 'compile-command)
+                               "go test && go build"))))
+        (let* ((guru (concat (getenv "GOPATH") "/src/golang.org/x/tools/cmd/guru/go-guru"))
+               (guru-el (concat guru ".el")))
+          (if (file-exists-p guru-el)
+              (progn
+                (load guru)                 ; preferentially loads .elc, then .el
+                (add-hook 'go-mode-hook #'(lambda () (go-guru-hl-identifier-mode t)))
+                (defun go-set-scope-here ()
+                  (interactive)
+                  (let ((scope (file-name-directory (buffer-file-name))))
+                    (setq go-guru-scope scope))))
+            (message "No such file: %s" guru-el))))))
 
-  ;; (add-to-list 'yas-snippet-dirs (concat user-emacs-directory "lisp/yasnippet-go"))
-  )
+;; (add-to-list 'yas-snippet-dirs (concat user-emacs-directory "lisp/yasnippet-go"))
 
 (provide 'setup-go-mode)
 

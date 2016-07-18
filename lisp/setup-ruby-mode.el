0c6e198f3dea208522c727f25ba53f08570f054e
Karrick S. McDermott <kmcdermott@linkedin.com>
2016-07-18 22:03:27 +0000

    ruby-mode customizations

--- lisp/setup-ruby-mode.el
+++ lisp/setup-ruby-mode.el
@@ -1,28 +1,38 @@
-;; avoid ridiculous ruby indentation
-(setq ruby-deep-indent-paren nil)
+;;; setup-ruby-mode -- customizations for Ruby programming language
 
-(defun ruby--jump-to-test ()
-  (find-file
-   (replace-regexp-in-string
-    "/lib/" "/test/"
-    (replace-regexp-in-string
-     "/\\([^/]+\\).rb$" "/test_\\1.rb"
-     (buffer-file-name)))))
+;;; Commentary:
 
-(defun ruby--jump-to-lib ()
-  (find-file
-   (replace-regexp-in-string
-    "/test/" "/lib/"
-    (replace-regexp-in-string
-     "/test_\\([^/]+\\).rb$" "/\\1.rb"
-     (buffer-file-name)))))
+;;; Code:
 
-(defun ruby-jump-to-other ()
-  (interactive)
-  (if (string-match-p "/test/" (buffer-file-name))
-      (ruby--jump-to-lib)
-    (ruby--jump-to-test)))
-
-(define-key ruby-mode-map (kbd "C-c t") 'ruby-jump-to-other)
+(add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+(eval-after-load "ruby-mode"
+  #'(lambda ()
+      (flyspell-prog-mode)
+      (setq ruby-deep-indent-paren nil) ; avoid ridiculous ruby indentation
+      (defun ruby--jump-to-test ()
+        (find-file
+         (replace-regexp-in-string
+          "/lib/" "/test/"
+          (replace-regexp-in-string
+           "/\\([^/]+\\).rb$" "/test_\\1.rb"
+           (buffer-file-name)))))
+      (defun ruby--jump-to-lib ()
+        (find-file
+         (replace-regexp-in-string
+          "/test/" "/lib/"
+          (replace-regexp-in-string
+           "/test_\\([^/]+\\).rb$" "/\\1.rb"
+           (buffer-file-name)))))
+      (defun ruby-jump-to-other ()
+        (interactive)
+        (if (string-match-p "/test/" (buffer-file-name))
+            (ruby--jump-to-lib)
+          (ruby--jump-to-test)))
+      ;; (add-hook 'ruby-mode-hook
+      ;;           #'(lambda ()
+      ;;               (local-set-key (kbd "C-c C-j") 'ruby-jump-other)
+      ))
 
 (provide 'setup-ruby-mode)
+
+;;; setup-ruby-mode.el ends here

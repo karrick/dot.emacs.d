085ada708e34f90b2778161194fc3b4885d364bd
Karrick S. McDermott <kmcdermott@linkedin.com>
2016-07-18 22:06:14 +0000

    go-mode customizations simplified

--- lisp/setup-go-mode.el
+++ lisp/setup-go-mode.el
@@ -1,41 +1,39 @@
 ;;; setup-go-mode --- customizations for Go programming language
 
+;;; Commentary:
+
 ;;; Code:
 
-(require 'go-autocomplete)
+(require 'path)
+;; (require 'go-autocomplete)
 
+(setenv "GO15VENDOREXPERIMENT" "1")     ; won't need this much longer
 (let ((gopath (expand-file-name "~/go")))
   (prepend-path (concat gopath "/bin"))
-  (setenv "GOPATH" gopath)
-  (setenv "GO15VENDOREXPERIMENT" "1")   ; won't need this much longer
-  (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
-  (eval-after-load "go-mode"
-    #'(lambda ()
-        (let ((gofmter (find-first #'(lambda (item)
-                                       (executable-find item))
-                                   '("goimports"
-                                     "gofmt"))))
-          (setq gofmt-command gofmter))
-        (add-hook 'go-mode-hook
-                  #'(lambda ()
-                      (add-hook 'before-save-hook #'gofmt-before-save nil t)
-                      (flyspell-prog-mode)
-                      (setq fill-column 100)
-                      ;; (local-set-key (kbd "C-c C-j") 'godef-jump-other-window)
-                      (if (not (string-match "^go" compile-command))
-                          (set (make-local-variable 'compile-command)
-                               "go test && go build"))))
-        (let* ((guru (concat (getenv "GOPATH") "/src/golang.org/x/tools/cmd/guru/go-guru"))
-               (guru-el (concat guru ".el")))
-          (if (file-exists-p guru-el)
-              (progn
-                (load guru)                 ; preferentially loads .elc, then .el
-                (add-hook 'go-mode-hook #'(lambda () (go-guru-hl-identifier-mode t)))
-                (defun go-set-scope-here ()
-                  (interactive)
-                  (let ((scope (file-name-directory (buffer-file-name))))
-                    (setq go-guru-scope scope))))
-            (message "No such file: %s" guru-el))))))
+  (setenv "GOPATH" gopath))
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+
+(with-eval-after-load 'go-mode
+  (setq gofmt-command (or (executable-find "goimports")
+                          (executable-find "gofmt")))
+  (let ((dir (concat (getenv "GOPATH") "/src/golang.org/x/tools/cmd/guru")))
+    (if (file-directory-p dir)
+        (progn
+          (add-to-list 'load-path dir)
+          (require 'go-guru)
+          (add-hook 'go-mode-hook #'(lambda () (go-guru-hl-identifier-mode t)))
+          (defun go-set-scope-here ()
+            (interactive)
+            (setq go-guru-scope (file-name-directory (buffer-file-name))))))
+    (message "Cannot find go-guru: %s" dir))
+  (add-hook 'go-mode-hook #'(lambda ()
+                              (add-hook 'before-save-hook #'gofmt-before-save nil t)
+                              (flyspell-prog-mode)
+                              (setq fill-column 100)
+                              (auto-complete-mode 1)
+                              ;; (local-set-key (kbd "C-c C-j") 'godef-jump-other-window)
+                              (if (not (string-match "^go" compile-command))
+                                  (set (make-local-variable 'compile-command) "go test && go build")))))
 
 ;; (add-to-list 'yas-snippet-dirs (concat user-emacs-directory "lisp/yasnippet-go"))
 

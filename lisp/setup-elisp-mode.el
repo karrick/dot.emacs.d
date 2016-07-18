111b87f816f7464bb568eba2e3753ea4d4cd5948
Karrick S. McDermott <kmcdermott@linkedin.com>
2016-07-18 22:05:23 +0000

    customization for emacs-lisp-mode

--- lisp/setup-elisp-mode.el
+++ lisp/setup-elisp-mode.el
@@ -1,14 +1,25 @@
 ;;; setup-elisp-mode -- customizations for Emacs Lisp programming language
 
+;;; Commentary:
+
 ;;; Code:
 
+(require 'clean-and-indent)
+
 (defun auto-recompile-el-buffer ()
+  "Recompiles Emacs Lisp file after if byte compiled file already exists."
   (interactive)
   (when (and (eq major-mode 'emacs-lisp-mode)
              (file-exists-p (byte-compile-dest-file buffer-file-name)))
     (byte-compile-file buffer-file-name)
     (message "Byte compiled %s" buffer-file-name)))
-(add-hook 'after-save-hook 'auto-recompile-el-buffer)
+
+(add-hook 'emacs-lisp-mode-hook
+          #'(lambda ()
+              (add-hook 'after-save-hook #'auto-recompile-el-buffer)
+              (add-hook 'before-save-hook #'clean-and-indent nil t)
+              (flyspell-prog-mode)
+              (setq fill-column 100)))
 
 (provide 'setup-elisp-mode)
 

22c91258f3724473250ff73f2b10126e8cac4999
Karrick S. McDermott <kmcdermott@linkedin.com>
2016-07-18 22:02:41 +0000

    replace setup-js2-mode with setup-javascript-mode

---
+++ lisp/setup-javascript-mode.el
@@ -0,0 +1,23 @@
+;;; setup-javascript-mode -- customizations for JavaScript programming language
+
+;;; Code:
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(autoload 'js2-mode "js2-mode" "Major mode for editing JavaScript code." t)
+(eval-after-load "js2-mode"
+  #'(lambda ()
+      ;; (defvaralias 'js-indent-level 'tab-width)
+      (set-default 'js2-basic-offset 4)
+      (set-default 'js2-mirror-mode nil)
+      (set-default 'js2-mode-escape-quotes nil)
+      (add-hook 'javascript-mode-hook #'(lambda ()
+                                          (add-hook 'before-save-hook #'clean-and-indent nil t)
+                                          (flyspell-prog-mode)
+                                          (setq fill-column 100
+                                                indent-tabs-mode t
+                                                ;; js-indent-level 4
+                                                )))))
+
+(provide 'setup-javascript-mode)
+
+;;; setup-javascript-mode.el ends here

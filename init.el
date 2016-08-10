3645b42720d99de01712c374283630ddd9da0e19
Karrick S. McDermott <kmcdermott@linkedin.com>
2016-08-10 02:00:51 -0700

    binds C-x C-f to custom find-file wrapper

--- init.el
+++ init.el
@@ -179,6 +179,8 @@ If there is no .svn directory, examine if there is CVS and run
 
 (add-hook 'after-init-hook
           #'(lambda ()
+              (require 'ido)
+              ;; (ido-mode 1)
               (ivy-mode 1)))
 
 ;; codesearch
@@ -190,12 +192,12 @@ If there is no .svn directory, examine if there is CVS and run
 
 ;;;; key bindings
 
-;; (global-set-key (kbd "C-x C-f") #'(lambda (&optional arg)
-;;                                     (interactive "P")
-;;                                     (if (equal current-prefix-arg nil)
-;;                                         (find-file-in-repository)
-;;                                         ; alternative: (ido-find-file)
-;;                                       (find-file "."))))
+(global-set-key (kbd "C-x C-f") #'(lambda (&optional arg)
+                                    (interactive "P")
+                                    (if (equal current-prefix-arg nil)
+                                        (find-file-in-repository)
+                                      (ido-find-file))))
+;; (find-file))))
 
 (global-set-key (kbd "C-x C-b") #'ibuffer)
 (global-set-key (kbd "C-x C-r") #'rgrep)

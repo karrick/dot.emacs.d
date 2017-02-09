dfc7b4674c37ce87f2bdda24199bb4003a42a1ae
Karrick S. McDermott <kmcdermott@linkedin.com>
2017-02-09 13:15:26 -0500

    removed test functions

--- lisp/require-package.el
+++ lisp/require-package.el
@@ -18,8 +18,8 @@
 (defun require-package/alist-from-tuples (tuples)
   "Convert TUPLES to alist of first two items in each tuple."
   (mapcar #'(lambda (tuple)
-	      (cons (car tuple) (cadr tuple)))
-	  tuples))
+              (cons (car tuple) (cadr tuple)))
+          tuples))
 
 (defvar require-package/package-contents-refreshed nil)
 
@@ -35,30 +35,30 @@ Elements from B will override any corresponding element from A."
   (let ((result nil))
     (dolist (item b)
       (unless (assoc (car item) result)
-	(setq result (cons item result))))
+        (setq result (cons item result))))
     (dolist (item a)
       (unless (assoc (car item) result)
-	(setq result (cons item result))))
+        (setq result (cons item result))))
     result))
 
 (defun require-package/pin-package (dotted-pair)
   "Prepend package DOTTED-PAIR to package-pinned-packages if not yet a member."
   (add-to-list 'package-pinned-packages dotted-pair nil
-	       #'(lambda (a b) (equal (car a) (car b)))))
+               #'(lambda (a b) (equal (car a) (car b)))))
 
 (defun require-package/value-from-key (key set)
   "Return element following KEY in the SET, or nil if not present."
   (cond ((eq set nil) nil)
-	((eq key (car set)) (cadr set))
-	(t (require-package/value-from-key key (cdr set)))))
+        ((eq key (car set)) (cadr set))
+        (t (require-package/value-from-key key (cdr set)))))
 
 (defmacro require-package/with-variable (name value &rest body)
   "Sets variable NAME with VALUE, then executes BODY.  Restores NAME to original value prior to returning."
   (declare (indent 1))
   `(let* ((saved ,name)
-	  (,name ,value))
+          (,name ,value))
      (unwind-protect
-	 ,@body
+         ,@body
        (setq ,name saved))))
 
 (defmacro require-package/with-package-archives (archives &rest body)
@@ -99,7 +99,7 @@ Elements from B will override any corresponding element from A."
       (require-package/alist-from-tuples
        ;; filter tuples to list where third item is nil
        (cl-remove-if #'(lambda (tuple) (nth 2 tuple))
-		     require-package/archive-tuples)))
+                     require-package/archive-tuples)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -132,7 +132,7 @@ Elements from B will override any corresponding element from A."
   (if (atom package-tuple)
       package-tuple
     (or (require-package/value-from-key :package package-tuple)
-	(car package-tuple))))		; package name defaults to FEATURE name
+        (car package-tuple))))		; package name defaults to FEATURE name
 
 (defun require-package/archive-from-package-tuple (package-tuple)
   "Return archive slot of PACKAGE-TUPLE."
@@ -145,7 +145,7 @@ Elements from B will override any corresponding element from A."
   (if (atom package-tuple)
       (cons package-tuple nil)
     (cons (require-package/package-from-package-tuple package-tuple)
-	  (require-package/archive-from-package-tuple package-tuple))))
+          (require-package/archive-from-package-tuple package-tuple))))
 
 (defcustom require-package/signature-requirement-default nil
   "Value used for package-check-signature when installing feature without specified :signature-requirement keyword.")
@@ -155,12 +155,12 @@ Elements from B will override any corresponding element from A."
   (if (atom package-tuple)
       require-package/signature-requirement-default
     (or (require-package/value-from-key :signature-requirement package-tuple)
-	require-package/signature-requirement-default)))
+        require-package/signature-requirement-default)))
 
 (defun require-package/pin-package-tuple (package-tuple)
   "Add PACKAGE-TUPLE to package-pinned-packages if not yet a member."
   (add-to-list 'package-pinned-packages (require-package/dotted-pair-from-package-tuple package-tuple) nil
-	       #'(lambda (a b) (equal (car a) (car b)))))
+               #'(lambda (a b) (equal (car a) (car b)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; *-from-package-tuples functions
@@ -168,15 +168,15 @@ Elements from B will override any corresponding element from A."
 (defun require-package/pinned-package-alist-from-package-tuples (package-tuples)
   "Return alist of PACKAGE-TUPLES which are pinned."
   (cl-remove-if #'(lambda (dotted-pair) (null (cdr dotted-pair)))
-		(mapcar #'require-package/dotted-pair-from-package-tuple package-tuples)))
+                (mapcar #'require-package/dotted-pair-from-package-tuple package-tuples)))
 
 (defmacro require-package/with-merged-pinned-packages (package-tuples &rest body)
   "Merge PACKAGE-TUPLES with package-pinned-packages and execute BODY."
   (declare (indent 1))
   `(require-package/with-package-pinned-packages
        (require-package/merge-alists
-	package-pinned-packages
-	(require-package/pinned-package-alist-from-package-tuples ,package-tuples))
+        package-pinned-packages
+        (require-package/pinned-package-alist-from-package-tuples ,package-tuples))
      ,@body))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -190,38 +190,29 @@ install, stop processing PACKAGE-TUPLES and return nil."
   (require-package/with-merged-pinned-packages package-tuples
     (catch 'missing-requirement
       (dolist (pt package-tuples)
-    	(let ((feature (require-package/feature-from-package-tuple pt))
-    	      (package-name (require-package/package-from-package-tuple pt))
-    	      (archive-name (require-package/archive-from-package-tuple pt))
-    	      (signature (require-package/signature-requirement-from-package-tuple pt)))
-    	  (unless
-    	      (or
-    	       (require feature nil 'no-error)
-    	       (y-or-n-p (format "Install package (%s)%s? "
-    				 package-name
-    				 (if (null archive-name)
-    				     ""
-    				   (format " from (%s)" archive-name)))))
-    	    (throw 'missing-requirement nil))
-    	  (condition-case err
-    	      (progn
-    		(require-package/maybe-package-refresh-contents)
-    		(require-package/with-package-check-signature signature
-    		  (package-install package-name))
-    		(require feature))
-    	    (error (progn
-    		     (message "Cannot install package: %s: %s" package-name err)
-    		     (throw 'missing-requirement nil))))))
+        (let ((feature (require-package/feature-from-package-tuple pt))
+              (package-name (require-package/package-from-package-tuple pt))
+              (archive-name (require-package/archive-from-package-tuple pt))
+              (signature (require-package/signature-requirement-from-package-tuple pt)))
+          (unless
+              (or
+               (require feature nil 'no-error)
+               (y-or-n-p (format "Install package (%s)%s? "
+                                 package-name
+                                 (if (null archive-name)
+                                     ""
+                                   (format " from (%s)" archive-name)))))
+            (throw 'missing-requirement nil))
+          (condition-case err
+              (progn
+                (require-package/maybe-package-refresh-contents)
+                (require-package/with-package-check-signature signature
+                  (package-install package-name))
+                (require feature))
+            (error (progn
+                     (message "Cannot install package: %s: %s" package-name err)
+                     (throw 'missing-requirement nil))))))
       t)))
-
-(defun test-ensure-require ()
-  (interactive)
-  (message "ensure-require return: %s"
-	   (require-package/ensure-require
-	    '(
-	      (go-mode)
-	      (go-eldoc :archive "melpa-stable")
-	      (go-autocomplete :archive "melpa")))))
 
 (defmacro require-package/with-requirements (package-tuples &rest body)
   "Requires PACKAGE-TUPLES then executes BODY.
@@ -233,16 +224,6 @@ loaded, it bails."
   (declare (indent 1))
   `(when (require-package/ensure-require ,package-tuples)
      ,@body))
-
-(defun test-with-requirements ()
-  (interactive)
-  (require-package/with-requirements
-      '(
-	(go-mode)
-	(go-eldoc :archive "melpa-stable")
-	(go-autocomplete :archive "melpa")
-	)
-    (message "everything installed")))
 
 (provide 'require-package)
 

2dd203880d7cf349cc350b1143cce679c256471c
Karrick S. McDermott <kmcdermott@linkedin.com>
2017-02-21 16:05:21 -0500

    cleanup and refactor around package-pinned-packages support

--- lisp/require-package.el
+++ lisp/require-package.el
@@ -24,6 +24,10 @@
 (defvar require-package/package-contents-refreshed nil)
 
 (defun require-package/maybe-package-refresh-contents ()
+  "Refresh package contents if not yet refreshed.
+
+Prevents every 'ensure-require' from triggering invocation of
+'package-refresh-contents'."
   (unless require-package/package-contents-refreshed
     (package-refresh-contents)
     (setq require-package/package-contents-refreshed (current-time-string))))
@@ -42,9 +46,13 @@ Elements from B will override any corresponding element from A."
     result))
 
 (defun require-package/pin-package (dotted-pair)
-  "Prepend package DOTTED-PAIR to package-pinned-packages if not yet a member."
-  (add-to-list 'package-pinned-packages dotted-pair nil
-               #'(lambda (a b) (equal (car a) (car b)))))
+  "Prepend package DOTTED-PAIR to 'package-pinned-packages' if not yet a member."
+  (cl-pushnew dotted-pair package-pinned-packages :key #'car))
+
+(defun require-package/pin-package-tuples (package-tuples)
+  "Add PACKAGE-TUPLES to 'package-pinned-packages' if not yet a member."
+  (dolist (dotted-pair (require-package/pinned-package-alist-from-package-tuples package-tuples))
+    (require-package/pin-package dotted-pair)))
 
 (defun require-package/value-from-key (key set)
   "Return element following KEY in the SET, or nil if not present."
@@ -53,7 +61,7 @@ Elements from B will override any corresponding element from A."
         (t (require-package/value-from-key key (cdr set)))))
 
 (defmacro require-package/with-variable (name value &rest body)
-  "Sets variable NAME with VALUE, then executes BODY.  Restores NAME to original value prior to returning."
+  "Set variable NAME with VALUE, then execute BODY.  Restore NAME to original value prior to return."
   (declare (indent 1))
   `(let* ((saved ,name)
           (,name ,value))
@@ -62,23 +70,23 @@ Elements from B will override any corresponding element from A."
        (setq ,name saved))))
 
 (defmacro require-package/with-package-archives (archives &rest body)
-  "Temporarily set package-archives to ARCHIVES and execute BODY."
+  "Temporarily set 'package-archives' to ARCHIVES and execute BODY."
   (declare (indent 1))
   `(require-package/with-variable package-archives ,archives ,@body))
 
 (defmacro require-package/with-package-check-signature (signature &rest body)
-  "Temporarily set package-check-signature set to SIGNATURE and execute BODY."
+  "Temporarily set 'package-check-signature' set to SIGNATURE and execute BODY."
   ;; valid options: t nil 'allow-unsigned
   (declare (indent 1))
   `(require-package/with-variable package-check-signature ,signature ,@body))
 
 (defmacro require-package/with-package-pinned-packages (pinned-package-alist &rest body)
-  "Temporarily set package-pinned-packages to PINNED-PACKAGE-ALIST and execute BODY."
+  "Temporarily set 'package-pinned-packages' to PINNED-PACKAGE-ALIST and execute BODY."
   (declare (indent 1))
   `(require-package/with-variable package-pinned-packages ,pinned-package-alist ,@body))
 
 (defmacro require-package/with-package-unsigned-archives (archives &rest body)
-  "Temporarily set package-unsigned-archives to ARCHIVES and execute BODY."
+  "Temporarily set 'package-unsigned-archives' to ARCHIVES and execute BODY."
   (declare (indent 1))
   `(require-package/with-variable package-unsigned-archives ,archives ,@body))
 
@@ -157,11 +165,6 @@ Elements from B will override any corresponding element from A."
     (or (require-package/value-from-key :signature-requirement package-tuple)
         require-package/signature-requirement-default)))
 
-(defun require-package/pin-package-tuple (package-tuple)
-  "Add PACKAGE-TUPLE to package-pinned-packages if not yet a member."
-  (add-to-list 'package-pinned-packages (require-package/dotted-pair-from-package-tuple package-tuple) nil
-               #'(lambda (a b) (equal (car a) (car b)))))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; *-from-package-tuples functions
 
@@ -170,14 +173,7 @@ Elements from B will override any corresponding element from A."
   (cl-remove-if #'(lambda (dotted-pair) (null (cdr dotted-pair)))
                 (mapcar #'require-package/dotted-pair-from-package-tuple package-tuples)))
 
-(defun require-package/merge-pinned-packages (package-tuples)
-  "Merge PACKAGE-TUPLES with package-pinned-packages.
-
-Elements from package-pinned-packages will override corresponding elements from PACKAGE-TUPLES."
-  (dolist (package-tuple (require-package/pinned-package-alist-from-package-tuples package-tuples))
-    (cl-pushnew package-tuple package-pinned-packages :key #'car)))
-
-(defmacro require-package/with-merged-pinned-packages (package-tuples &rest body)
+(defmacro require-package/with-merged-pinned-packages-from-package-tuples (package-tuples &rest body)
   "Merge PACKAGE-TUPLES with package-pinned-packages and execute BODY."
   (declare (indent 1))
   `(require-package/with-package-pinned-packages
@@ -194,7 +190,7 @@ Elements from package-pinned-packages will override corresponding elements from 
 If any package-tuple cannot be loaded, prompt user to install
 corresponding package. If user declines or package fails to
 install, stop processing PACKAGE-TUPLES and return nil."
-  (require-package/merge-pinned-packages package-tuples)
+  (require-package/pin-package-tuples package-tuples)
   (catch 'missing-requirement
     (dolist (pt package-tuples)
       (let ((feature (require-package/feature-from-package-tuple pt))

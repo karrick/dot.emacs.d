70f9cdbb3ea49ae5e1256f2ad13114fc4fe7f8f3
Karrick S. McDermott <karrick@karrick.net>
2019-02-01 19:50:36 +0000

    better windnow zoom toggling

--- lisp/ksm-window-toggle.el
+++ lisp/ksm-window-toggle.el
@@ -2,21 +2,29 @@
 
 ;;; Commentary:
 
+;;; This package uses register 0 to store the window layout when zooming in.
+
 ;;; Code:
 
-(setq ksm/window-toggle-zoomed nil)
+(defvar ksm/window-toggle-zoomed nil "Internal variable that is non-nil when zoomed from multiple windows.")
 
 (defun ksm/window-toggle ()
   "Toggle current frame between single window and multiple windows."
   (interactive)
+
   (if ksm/window-toggle-zoomed
       (progn
         (jump-to-register ?0)
-        (setq ksm/window-toggle-zoomed nil))
-    (progn
-      (window-configuration-to-register ?0)
-      (delete-other-windows)
-      (setq ksm/window-toggle-zoomed t))))
+        (setq ksm/window-toggle-zoomed nil)
+        (message "restored to previously saved window layout"))
+    (if (eq 1 (length (window-list)))
+        (message "cannot zoom when already single window")
+      (progn
+        ;; When not zoomed in, and multiple windows, then save this layout.
+        (window-configuration-to-register ?0)
+        (delete-other-windows)
+        (setq ksm/window-toggle-zoomed t)
+        (message "zoomed in from multiple window layout")))))
 
 (provide 'ksm-window-toggle)
 

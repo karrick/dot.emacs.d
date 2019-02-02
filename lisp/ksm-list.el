4a441ac6dfc04d3e6666b25e456b86143eb48af4
Karrick S. McDermott <karrick@karrick.net>
2019-02-02 01:13:44 +0000

    ksm-list.el

---
+++ lisp/ksm-list.el
@@ -0,0 +1,25 @@
+(defun ksm/drop (n list)
+  "Drop the first N items from LIST and return the rest."
+  (if (and (> n 0) list)
+      (ksm/drop (1- n) (cdr list))
+    list))
+
+(defun ksm/take (n list)
+  "Return the first N items from LIST. Returns empty list when N is 0."
+  (if (and (> n 0) list)
+      (cons (car list) (ksm/take (1- n) (cdr list)))))
+
+(defun ksm/list-insert-at-index (element list i)
+  "Insert ELEMENT into LIST at (zero-based) index I."
+  (let* ((length (length list))
+         (left (min length (max 0 i))))
+    (append (ksm/take left list)
+            (cons element (ksm/drop left list)))))
+
+(defun ksm/list-remove-at-index (i list)
+  "Remove the element at (zero-based) index I from LIST."
+  (append
+   (ksm/take i list)
+   (ksm/drop (1+ i) list)))
+
+(provide 'ksm-list)

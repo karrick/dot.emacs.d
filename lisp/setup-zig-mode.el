200092595c672042fbb6dde123c6832a11c411d9
Karrick S. McDermott <kmcdermott@linkedin.com>
2019-08-22 15:03:45 -0400

    make zig mode happen

--- lisp/setup-zig-mode.el
+++ lisp/setup-zig-mode.el
@@ -24,11 +24,11 @@
   ;;       (shell-command-on-
   ;;     )
 
-  ;; ;; This block sets up buffer scoped configuration and is invoked every time a new go-mode buffer is created.
-  ;; (add-hook 'zig-mode-hook #'(lambda ()
-  ;;                             (add-hook 'before-save-hook #'zigfmt-before-save nil t)
-  ;;                             (local-set-key (kbd "C-c C-d") #'godoc-at-point)
-  ;;                             (local-set-key (kbd "M-.") #'godef-jump-other-window)
-  ;;                             (set (make-local-variable 'compile-command) "zig test")))
+  ;; ;; This block sets up buffer scoped configuration and is invoked every time a new zig-mode buffer is created.
+  (add-hook 'zig-mode-hook #'(lambda ()
+                               ;; (add-hook 'before-save-hook #'zigfmt-before-save nil t)
+                               ;; (local-set-key (kbd "C-c C-d") #'godoc-at-point)
+                               ;; (local-set-key (kbd "M-.") #'godef-jump-other-window)
+                               (set (make-local-variable 'compile-command) (concat "zig test " (buffer-file-name)))))
 
-  ;; (add-to-list 'auto-mode-alist '("\\.zig\\'" . zig-mode))
+  (add-to-list 'auto-mode-alist '("\\.zig\\'" . zig-mode)))

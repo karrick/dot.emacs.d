09e03727470f8a39e1d3c3c33c297f783e5777e4
Karrick S. McDermott <karrick@karrick.dev>
2020-08-04 10:11:06 -0400

    ansi compilation buffer

--- init.el
+++ init.el
@@ -282,6 +282,17 @@ If there is no .svn directory, examine if there is CVS and run
   (default-text-scale-mode))
 
 (when (display-color-p)
+
+  ;; compilation buffers should convert ANSI sequences to color formatting
+  (require 'ansi-color)
+  (defun endless/colorize-compilation ()
+    "Colorize from `compilation-filter-start' to `point'."
+    (let ((inhibit-read-only t))
+      (ansi-color-apply-on-region
+       compilation-filter-start (point))))
+  (add-hook 'compilation-filter-hook
+            #'endless/colorize-compilation)
+
   (require-package/with-requirements '(zenburn-theme)
     (load-theme 'zenburn t))
   (require-package/with-requirements '(xterm-color)

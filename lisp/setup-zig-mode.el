f274d96a45d382f406c1c1fd63fcf09644ef6dd1
Karrick S. McDermott <karrick@karrick.dev>
2021-06-30 16:29:50 -0400

    setup-zig-mode handles missing zls gracefully

--- lisp/setup-zig-mode.el
+++ lisp/setup-zig-mode.el
@@ -9,19 +9,20 @@
 (require 'require-package)
 
 (require-package/with-requirements '(zig-mode)
-  (require 'lsp)
-  (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
-  (lsp-register-client
-   (make-lsp-client
-    :new-connection (lsp-stdio-connection (executable-find "zls"))
-    :major-modes '(zig-mode)
-    :server-id 'zls))
+  (let ((cmd (executable-find "zls")))
+    (when cmd
+      (require 'lsp)
+      (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
+      (lsp-register-client
+       (make-lsp-client
+	:new-connection (lsp-stdio-connection (executable-find "zls"))
+	:major-modes '(zig-mode)
+	:server-id 'zls))))
 
   ;; ;; This block sets up buffer scoped configuration and is invoked every time a new zig-mode buffer is created.
   (add-hook 'zig-mode-hook
-            #'(lambda ()
-                (set (make-local-variable 'compile-command) (concat "zig test " (buffer-file-name)))))
-
+	    #'(lambda ()
+		(set (make-local-variable 'compile-command) (concat "zig test " (buffer-file-name)))))
   (add-to-list 'auto-mode-alist '("\\.zig\\'" . zig-mode)))
 
 (provide 'setup-zig-mode)

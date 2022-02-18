7d176186f0e54665c9970791f0b4f381d9de6aeb
Karrick S. McDermott <karrick@karrick.dev>
2022-02-17 23:20:04 -0500

    fixing up setup-golang-mode

--- lisp/setup-golang-mode.el
+++ lisp/setup-golang-mode.el
@@ -4,9 +4,10 @@
 
 ;;; Code:
 
-;; configure emacs and environment for the Go programming language
+;; configure emacs and environment for the Go programming language.
 
 (require 'path)
+
 (let ((gopath (expand-file-name "~/go")))
   (path-prepend (concat gopath "/bin"))
   ;; (setenv "GOBIN" (expand-file-name "~/bin"))
@@ -15,81 +16,83 @@
 (require 'require-package)
 
 (require-package/with-requirements '(go-mode)
-
-  ;; display messages for missing command line tools that don't have
-  ;; associated required packages
+  ;; Display messages for missing command line tools that don't have
+  ;; associated required packages.
   (dolist (tuple '(
                    ("godef" "github.com/rogpeppe/godef")
                    ("maligned" "github.com/mdempsky/maligned") ; ???
+                   ("staticcheck" "honnef.co/go/tools/cmd/staticcheck")
                    ("unconvert" "github.com/mdempsky/unconvert")
                    ))
     (unless (executable-find (car tuple))
-      (message "Cannot find %s: `go install %s@latest`" (car tuple) (cadr tuple))))
+      (message "Cannot find %s: 'go install %s@latest'" (car tuple) (cadr tuple))))
 
-  ;; prefer goimports, but if not found, display message and use gofmt
+  ;; Prefer goimports, but if not found, display message and use
+  ;; gofmt.
   (setq gofmt-command (or (executable-find "goimports")
                           (progn
-                            (message "Cannot find goimports: `go install golang.org/x/tools/cmd/goimports@latest`")
+                            (message "Cannot find goimports: 'go install golang.org/x/tools/cmd/goimports@latest'")
                             (executable-find "gofmt"))))
 
   ;; go-eldoc -- eldoc for the Go programming language
+  ;; TODO: looking at go-eldoc code, looks like it uses 'gocode' binary.
   (require-package/with-requirements '(go-eldoc)
     (set-face-attribute 'eldoc-highlight-function-argument nil
                         :underline t :foreground "green"
                         :weight 'bold)
     (add-hook 'go-mode-hook #'go-eldoc-setup))
 
-  ;; go-autocomplete
-  (require-package/with-requirements '(go-autocomplete)
-    ;; gocode -- autocompletion daemon for the Go programming language (requires go-autocomplete)
-    (let ((dir (path-concat (getenv "GOPATH") "src/github.com/nsf/gocode/emacs")))
-      (if (file-directory-p dir)
-          (progn
-            (add-to-list 'load-path dir)
-            (ac-config-default)
-            (add-hook 'go-mode-hook #'(lambda () (auto-complete-mode 1))))
-        (message "Cannot find gocode: `go install github.com/nsf/gocode@latest`"))))
-
   ;; gogetdoc: provides better documentation
   (let ((cmd (executable-find "gogetdoc")))
-    (if (string-equal cmd "")
-        (message "Cannot find gogetdoc: `go install github.com/zmb3/gogetdoc@latest`")
+    (if (equal cmd nil)
+        (message "Cannot find gogetdoc: 'go install github.com/zmb3/gogetdoc@latest'")
       (setq godoc-at-point-function #'godoc-gogetdoc)))
 
-  ;; gorename
-  (let ((cmd (executable-find "gorename")))
-    (if (string-equal cmd "")
-        (message "Cannot find gorename: `go install golang.org/x/tools/cmd/gorename@latest`")
-      (require-package/with-requirements '(go-rename)
-        (setq go-rename-command cmd))))
+  ;; goflymake -- not sure whether this was causing problems in the
+  ;; past
+  (let ((cmd (executable-find "goflymake"))
+        (dir (path-concat (getenv "GOPATH") "src/github.com/dougm/goflymake")))
+    (if (or (equal cmd nil)
+            (not (file-directory-p dir)))
+        (message "Cannot find: 'mkdir -p ~/go/src/github.com/dougm && cd ~/go/src/github.com/dougm && git clone https://github.com/dougm/goflymake && cd goflymake && go install'")
+      (add-to-list 'load-path dir)
+      (require 'go-flycheck)))
 
-  ;; golint
-  (if (executable-find "golint")
-      (require-package/ensure-require '(golint))
-    (message "Cannot find golint: `go install golang.org/x/lint/golint@latest`"))
+  ;; ;; NOTE: I do not know whether gocode actually ever does anything...
+  ;; ;; go-autocomplete
+  ;; (require-package/with-requirements '(go-autocomplete)
+  ;;   ;; gocode -- autocompletion daemon for the Go programming language (requires go-autocomplete)
+  ;;   (let ((dir (path-concat (getenv "GOPATH") "src/github.com/nsf/gocode/emacs")))
+  ;;     (if (file-directory-p dir)
+  ;;         (progn
+  ;;           (add-to-list 'load-path dir)
+  ;;           (ac-config-default)
+  ;;           (add-hook 'go-mode-hook #'(lambda () (auto-complete-mode 1))))
+  ;;       (message "Cannot find gocode: 'go install github.com/nsf/gocode@latest'"))))
 
-  ;; guru
-  (let ((cmd (executable-find "guru")))
-    (if (not cmd)
-        (message "Cannot find guru: `go install golang.org/x/tools/cmd/guru@latest`")
-      (require-package/with-requirements '(go-guru)
-        (setq go-guru-command cmd)
-        (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
-        (set-face-attribute 'go-guru-hl-identifier-face nil
-                            :background "chartreuse"
-                            :foreground "gray0")
-        (defun go-set-scope-here ()
-          (interactive)
-          (setq go-guru-scope (file-name-directory (buffer-file-name)))))))
-
-  ;; This block sets up buffer scoped configuration and is invoked every time a new go-mode buffer is created.
+  ;; This block sets up buffer scoped configuration and is invoked
+  ;; every time a new go-mode buffer is created.
   (add-hook 'go-mode-hook #'(lambda ()
                               (add-hook 'before-save-hook #'gofmt-before-save nil t)
+                              ;; (set (make-local-variable 'compile-command) "go test")
                               (local-set-key (kbd "C-c C-d") #'godoc-at-point)
-                              (local-set-key (kbd "M-.") #'godef-jump-other-window)
-                              (set (make-local-variable 'compile-command) "go test")))
+                              (local-set-key (kbd "M-.") #'godef-jump-other-window)))
 
   (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode)))
+
+(when nil
+  (custom-set-variables
+   '(go-add-tags-style 'lower-camel-case))
+  (with-eval-after-load 'go-mode
+    (define-key go-mode-map (kbd "C-c t") #'go-add-tags)))
+
+(when nil
+  (add-mode-hook 'go-mode'hook (lambda ()
+                                 (setq tab-width 4))))
+
+;; % go get golang.org/x/tools/cmd/gorename
+;; % go build golang.org/x/tools/cmd/gorename
+;; % mv gorename $HOME/bin/         # or elsewhere on $PATH
 
 (provide 'setup-golang-mode)
 

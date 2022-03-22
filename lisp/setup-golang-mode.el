856c509e5d85144af1b62925779a5ae8aa1e0d8b
Karrick S. McDermott <karrick@karrick.dev>
2022-03-22 15:49:20 -0400

    working on go-mode + lsp-mode + gopls

--- lisp/setup-golang-mode.el
+++ lisp/setup-golang-mode.el
@@ -6,35 +6,45 @@
 
 ;; Configure emacs and environment for the Go programming language.
 
-(require 'path)
-
-(let ((gopath (expand-file-name "~/go")))
-  (path-prepend (concat gopath "/bin"))
-  ;; (setenv "GOBIN" (expand-file-name "~/bin"))
-  (setenv "GOPATH" gopath))
-
 (require 'require-package)
 
 (require-package/with-requirements '(go-mode)
   (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
 
-  ;; Prefer goimports, but when not found, use gofmt.
-  (setq gofmt-command (or (executable-find "goimports")
-                          (executable-find "gofmt")))
+  (require 'path)
+  (let ((gopath (expand-file-name "~/go")))
+    (path-prepend (concat gopath "/bin"))
+    ;; (setenv "GOBIN" (expand-file-name "~/bin"))
+    (setenv "GOPATH" gopath))
 
   ;; Use gogetdoc as it provides better documentation.
   (when (executable-find "gogetdoc")
     (setq godoc-at-point-function #'godoc-gogetdoc))
 
-  (when (executable-find "gopls")
-    (require-package/with-requirements '(lsp-mode)
-      (add-hook 'go-mode-hook #'lsp-deferred)))
+  (let ((cmd (executable-find "gopls")))
+    (if (stringp cmd)
+        (progn
+          ;; When gopls executable is found, install hooks for go-mode
+          ;; to use it.
+          (require 'lsp-mode)
+          (setq lsp-go-gopls-server-path cmd)
+          (add-hook 'go-mode-hook #'lsp-deferred)
+          ;; Set up before-save hooks to format buffer and add/delete
+          ;; imports. Make sure there is not another gofmt or
+          ;; goimports hook enabled.
+          (defun lsp-go-install-save-hooks ()
+            (add-hook 'before-save-hook #'lsp-format-buffer t t)
+            (add-hook 'before-save-hook #'lsp-organize-imports t t))
+          (add-hook 'go-mode-hook #'lsp-go-install-save-hooks))
+      (progn
+        ;; When cannot find gopls language server, configure a different
+        ;; go-mode-hook for graceful feature degredation.
 
-  ;; This block sets up buffer scoped configuration and is invoked
-  ;; every time a new go-mode buffer is created.
-  (add-hook 'go-mode-hook #'(lambda ()
-                              ;; (set (make-local-variable 'compile-command) "go test")
-                              (add-hook 'before-save-hook #'gofmt-before-save nil t))))
+        ;; Prefer goimports, but when not found, use gofmt.
+        (setq gofmt-command (or (executable-find "goimports")
+                                (executable-find "gofmt")))
+        (add-hook 'go-mode-hook #'(lambda ()
+                                    (add-hook 'before-save-hook #'gofmt-before-save nil t)))))))
 
 (provide 'setup-golang-mode)
 

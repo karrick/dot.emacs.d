a092dcc96c949753d32fcb30552566db60ad89f3
Karrick S. McDermott <karrick@karrick.dev>
2022-04-07 14:03:55 -0400

    setup-golang-mode

--- lisp/setup-golang-mode.el
+++ lisp/setup-golang-mode.el
@@ -13,53 +13,51 @@
 
   (require 'path)
   (let ((gopath (expand-file-name "~/go")))
-    (path-prepend (concat gopath "/bin"))
-    ;; (setenv "GOBIN" (expand-file-name "~/bin"))
-    (setenv "GOPATH" gopath))
+	(setenv "GOPATH" gopath))
 
   ;; Use gogetdoc as it provides better documentation.
   (when (executable-find "gogetdoc")
-    (setq godoc-at-point-function #'godoc-gogetdoc))
+	(setq godoc-at-point-function #'godoc-gogetdoc))
 
   (when nil
-    ;; Fix parsing of error and warning lines in compiler output.
-    (setq compilation-error-regexp-alist-alist ; first remove the standard conf; it's not good.
-          (remove 'go-panic
-                  (remove 'go-test compilation-error-regexp-alist-alist)))
-    ;; Make another one that works better and strips more space at the beginning.
-    (add-to-list 'compilation-error-regexp-alist-alist
-                 '(go-test . ("^[[:space:]]*\\([_a-zA-Z./][_a-zA-Z0-9./]*\\):\\([0-9]+\\):.*$" 1 2)))
-    (add-to-list 'compilation-error-regexp-alist-alist
-                 '(go-panic . ("^[[:space:]]*\\([_a-zA-Z./][_a-zA-Z0-9./]*\\):\\([0-9]+\\)[[:space:]].*$" 1 2)))
-    ;; override.
-    (add-to-list 'compilation-error-regexp-alist 'go-test t)
-    (add-to-list 'compilation-error-regexp-alist 'go-panic t)
-    )
+	;; Fix parsing of error and warning lines in compiler output.
+	(setq compilation-error-regexp-alist-alist ; first remove the standard conf; it's not good.
+		  (remove 'go-panic
+				  (remove 'go-test compilation-error-regexp-alist-alist)))
+	;; Make another one that works better and strips more space at the beginning.
+	(add-to-list 'compilation-error-regexp-alist-alist
+				 '(go-test . ("^[[:space:]]*\\([_a-zA-Z./][_a-zA-Z0-9./]*\\):\\([0-9]+\\):.*$" 1 2)))
+	(add-to-list 'compilation-error-regexp-alist-alist
+				 '(go-panic . ("^[[:space:]]*\\([_a-zA-Z./][_a-zA-Z0-9./]*\\):\\([0-9]+\\)[[:space:]].*$" 1 2)))
+	;; override.
+	(add-to-list 'compilation-error-regexp-alist 'go-test t)
+	(add-to-list 'compilation-error-regexp-alist 'go-panic t)
+	)
 
   (let ((cmd (executable-find "gopls")))
-    (if (stringp cmd)
-        (progn
-          ;; When gopls executable is found, install hooks for go-mode
-          ;; to use it.
-          (require 'lsp-mode)
-          (setq lsp-go-gopls-server-path cmd)
-          (add-hook 'go-mode-hook #'lsp-deferred)
-          ;; Set up before-save hooks to format buffer and add/delete
-          ;; imports. Make sure there is not another gofmt or
-          ;; goimports hook enabled.
-          (defun lsp-go-install-save-hooks ()
-            (add-hook 'before-save-hook #'lsp-format-buffer t t)
-            (add-hook 'before-save-hook #'lsp-organize-imports t t))
-          (add-hook 'go-mode-hook #'lsp-go-install-save-hooks))
-      (progn
-        ;; When cannot find gopls language server, configure a different
-        ;; go-mode-hook for graceful feature degredation.
+	(if cmd
+		(progn
+		  ;; When gopls language server is found, install hooks for
+		  ;; go-mode to use it.
+		  (require 'lsp-mode)
+		  (setq lsp-go-gopls-server-path cmd)
+		  (add-hook 'go-mode-hook #'lsp-deferred)
+		  ;; Set up before-save hooks to format buffer and add/delete
+		  ;; imports. Make sure there is not another gofmt or
+		  ;; goimports hook enabled.
+		  (defun lsp-go-install-save-hooks ()
+			(add-hook 'before-save-hook #'lsp-format-buffer t t)
+			(add-hook 'before-save-hook #'lsp-organize-imports t t))
+		  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks))
+	  (progn
+		;; When cannot find gopls language server, configure a
+		;; different go-mode-hook for graceful feature degredation.
 
-        ;; Prefer goimports, but when not found, use gofmt.
-        (setq gofmt-command (or (executable-find "goimports")
-                                (executable-find "gofmt")))
-        (add-hook 'go-mode-hook #'(lambda ()
-                                    (add-hook 'before-save-hook #'gofmt-before-save nil t)))))))
+		;; Prefer goimports, but when not found, use gofmt.
+		(setq gofmt-command (or (executable-find "goimports")
+								(executable-find "gofmt")))
+		(add-hook 'go-mode-hook #'(lambda ()
+									(add-hook 'before-save-hook #'gofmt-before-save nil t)))))))
 
 (provide 'setup-golang-mode)
 

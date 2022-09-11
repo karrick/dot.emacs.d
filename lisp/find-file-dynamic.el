636d5e607f72852863137d461786c899f9a2b31a
Karrick S. McDermott <karrick@karrick.dev>
2022-09-11 19:27:12 -0400

    find-alternative-file-with-sudo

--- lisp/find-file-dynamic.el
+++ lisp/find-file-dynamic.el
@@ -10,10 +10,24 @@
   "C-x C-f invokes #'ido-file-file; with ARG, invokes #'find-file-in-repository."
   (interactive "P")
   (if (equal current-prefix-arg nil)
-      (ido-find-file)
-    (find-file-in-repository)))
+	  (ido-find-file)
+	(find-file-in-repository)))
 
 (global-set-key (kbd "C-x C-f") #'find-file-dynamic)
+
+;; From: https://www.emacswiki.org/emacs/TrampMode
+(defun find-alternative-file-with-sudo ()
+  "Toggle between opening the file with sudo or as unprivileged user."
+  (interactive)
+  (let ((fname (or buffer-file-name
+				   dired-directory)))
+	(when fname
+	  (if (string-match "^/sudo:root@localhost:" fname)
+		  (setq fname (replace-regexp-in-string
+					   "^/sudo:root@localhost:" ""
+					   fname))
+		(setq fname (concat "/sudo:root@localhost:" fname)))
+	  (find-alternate-file fname))))
 
 (provide 'find-file-dynamic)
 

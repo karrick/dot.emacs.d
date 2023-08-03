2f788a35c8a87b3398f758c8a0cbab3d6c748ea5
Karrick S. McDermott <karrick@karrick.net>
2023-08-03 17:08:27 -0400

    test-function

---
+++ lisp/test-function.el
@@ -0,0 +1,18 @@
+;;; test-function -- test function over a list of inputs and expected outputs
+
+;;; Commentary:
+
+;;; Code:
+
+(defun test-function (function cases)
+  "Test FUNCTION over the provided CASES."
+  (dolist (case cases)
+	(let* ((input (car case))
+		   (got (funcall function input))
+		   (want (cdr case)))
+	  (unless (equal got want)
+		(message "test-function: (%s %s) (GOT: %s; WANT: %s)" (symbol-name function) input got want)))))
+
+(provide 'test-function)
+
+;;; test-function.el ends here

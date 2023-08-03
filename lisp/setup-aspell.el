26a164942761fcc7ee1ab02976d3d25ff207c798
Karrick S. McDermott <karrick@karrick.net>
2023-08-03 18:03:51 -0400

    setup-aspell.el

---
+++ lisp/setup-aspell.el
@@ -0,0 +1,23 @@
+;;; setup-aspell -- configure and start `aspell(1)` helper program
+
+;;; Commentary:
+;;;
+;;; Based on suggestions found at
+;;; http://blog.binchen.org/posts/what-s-the-best-spell-check-set-up-in-emacs.html
+
+;;; Code:
+
+(require 'empty-string)
+
+(let ((cmd (executable-find "aspell")))
+  (if (null cmd)
+	  (message "Cannot find spelling program: consider installing 'aspell' and 'en-aspell' packages.")
+	(add-hook 'prog-mode-hook #'flyspell-prog-mode)
+	(setq ispell-program-name cmd
+		  ;; NOTE: ispell-extra-args contains actual parameters
+		  ;; that will be passed to aspell.
+		  ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"))))
+
+(provide 'setup-aspell)
+
+;;; setup-aspell.el ends here
